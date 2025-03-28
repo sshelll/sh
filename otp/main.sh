70933fa55e6296d7cd869a8e3dfa8da9f8541ab6
kayce <shaojiale.gitignore@icloud.com>
2025-03-28 21:48:11 +0800

    feat(otp): add makefile to install this tool.

---
+++ otp/main.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+main() {
+	if [[ $# -lt 1 ]]; then
+		echo "Usage: $0 {register|gen}"
+		exit 1
+	fi
+	subcommand="$1"
+	shift
+	case "$subcommand" in
+	gen)
+		gen_otp
+		;;
+	register)
+		register_otp
+		;;
+	*)
+		echo "unknown command: $subcommand"
+		;;
+	esac
+}
+
+main "$@"
---
+++ otp/otptool.plugin.zsh
@@ -0,0 +1,22 @@
+#compdef otptool
+
+_otptool_completions() {
+  local -a commands
+  commands=(
+    "gen:Gen otp code from registered otp keys"
+    "register:Register a new otp key"
+  )
+
+  local curcontext="$curcontext" state line
+  _arguments \
+    '1: :->subcommand' \
+    '*: :->args'
+
+  case $state in
+    subcommand)
+      _describe -t commands 'otptool commands' commands
+      ;;
+  esac
+}
+
+compdef _otptool_completions otptool
