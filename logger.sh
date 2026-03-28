1a711dd969fa77e2b0214aa1083b69d60825241d
kayce <shaojiale.gitignore@icloud.com>
2026-03-28 16:02:24 +0800

    feat: add utils.

--- logger.sh
+++ logger.sh
@@ -5,49 +5,54 @@ WARN=4
 ERROR=5
 
 function log_trace {
-	log $TRACE "$1"
+    log $TRACE "$1"
 }
 
 function log_debug {
-	log $DEBUG "$1"
+    log $DEBUG "$1"
 }
 
 function log_info {
-	log $INFO "$1"
+    log $INFO "$1"
 }
 
 function log_warn {
-	log $WARN "$1"
+    log $WARN "$1"
 }
 
 function log_error {
-	log $ERROR "$1"
+    log $ERROR "$1"
+}
+
+function log_success {
+    local date=$(date +"%Y-%m-%d %H:%M:%S:%3N %Z%z")
+    echo -e "$date \033[32m✔ $1\033[0m"
 }
 
 # log
 # $1: level
 # $2: message
 function log {
-	if [ $# -lt 2 ]; then
-		echo "Usage: log <level> <message>"
-		return
-	fi
+    if [ $# -lt 2 ]; then
+        echo "Usage: log <level> <message>"
+        return
+    fi
 
-	local level=$1
-	local message=$2
-	local date=$(date +"%Y-%m-%d %H:%M:%S:%3N %Z%z")
+    local level=$1
+    local message=$2
+    local date=$(date +"%Y-%m-%d %H:%M:%S:%3N %Z%z")
 
-	if [ $level -eq $TRACE ]; then
-		echo -e "$date \033[34mTRACE: $message\033[0m"
-	elif [ $level -eq $DEBUG ]; then
-		echo -e "$date \033[32mDEBUG: $message\033[0m"
-	elif [ $level -eq $INFO ]; then
-		echo -e "$date \033[36mINFO: $message\033[0m"
-	elif [ $level -eq $WARN ]; then
-		echo -e "$date \033[33mWARN: $message\033[0m"
-	elif [ $level -eq $ERROR ]; then
-		echo -e "$date \033[31mERROR: $message\033[0m"
-	else
-		echo -e "$date \033[31mUNKNOWN: $message\033[0m"
-	fi
+    if [ $level -eq $TRACE ]; then
+        echo -e "$date \033[34mTRACE: $message\033[0m"
+    elif [ $level -eq $DEBUG ]; then
+        echo -e "$date \033[32mDEBUG: $message\033[0m"
+    elif [ $level -eq $INFO ]; then
+        echo -e "$date \033[36mINFO: $message\033[0m"
+    elif [ $level -eq $WARN ]; then
+        echo -e "$date \033[33mWARN: $message\033[0m"
+    elif [ $level -eq $ERROR ]; then
+        echo -e "$date \033[31mERROR: $message\033[0m"
+    else
+        echo -e "$date \033[31mUNKNOWN: $message\033[0m"
+    fi
 }
--- utils.sh
+++ utils.sh
@@ -5,144 +5,166 @@
 # 	echo "ls command not found"
 # fi
 function check_command_exists {
-	local cmd=$1
-	if ! command -v "$cmd" >/dev/null 2>&1; then
-		return 1
-	fi
-	return 0
+    local cmd=$1
+    if ! command -v "$cmd" >/dev/null 2>&1; then
+        return 1
+    fi
+    return 0
 }
 
 # select with ui
 # $1: title
 # $2..n: items
 function select_ui {
-	if [ $# -lt 2 ]; then
-		echo_stderr "Usage: select_ui <title> <item1> <item2> ..."
-		exit 1
-	fi
+    if [ $# -lt 2 ]; then
+        echo_stderr "Usage: select_ui <title> <item1> <item2> ..."
+        exit 1
+    fi
 
-	local title=$1
-	shift 1
-	local args=("$@")
+    local title=$1
+    shift 1
+    local args=("$@")
 
-	echo_stderr "$title"
-	local idx=0
-	for item in "${args[@]}"; do
-		echo_stderr "$idx) $item"
-		idx=$((idx + 1))
-	done
+    echo_stderr "$title"
+    local idx=0
+    for item in "${args[@]}"; do
+        echo_stderr "$idx) $item"
+        idx=$((idx + 1))
+    done
 
-	local input=$(must_read "select")
-	while [[ $input -lt 0 || $input -ge $# ]]; do
-		input=$(must_read "select")
-	done
+    local input=$(must_read "select")
+    while [[ $input -lt 0 || $input -ge $# ]]; do
+        input=$(must_read "select")
+    done
 
-	echo "${args[$input]}"
+    echo "${args[$input]}"
 }
 
 function select_ui_termenu {
-	if [ $# -lt 2 ]; then
-		echo_stderr "Usage: select_ui <title> <item1> <item2> ..." >&2
-		exit 1
-	fi
+    if [ $# -lt 2 ]; then
+        echo_stderr "Usage: select_ui <title> <item1> <item2> ..." >&2
+        exit 1
+    fi
 
-	local title=$1
-	shift
-	local args=("$@")
+    local title=$1
+    shift
+    local args=("$@")
 
-	local items=$(printf "%s\n" "${args[@]}")
+    local items=$(printf "%s\n" "${args[@]}")
 
-	echo "$items" | termenu --name "$title" --color always
+    echo "$items" | termenu --name "$title" --color always
 }
 
 # unwrap or default, if arg is empty, use default
 # $1: arg
 # $2: default
 function unwrap_or_default {
-	local input=$1
-	local default=$2
-	if [[ -z "$input" ]]; then
-		echo "$default"
-	else
-		echo "$input"
-	fi
+    local input=$1
+    local default=$2
+    if [[ -z "$input" ]]; then
+        echo "$default"
+    else
+        echo "$input"
+    fi
 }
 
 # read or default, if input is empty, use default
 # $1: prompt
 # $2: default
 function read_or_default {
-	local prompt=$1
-	local default=$2
-	local input=''
-	read -p "> $prompt: " input
-	if [[ -z "$input" && -n "$default" ]]; then
-		input=$default
-	fi
-	echo "$input"
+    local prompt=$1
+    local default=$2
+    local input=''
+    read -p "> $prompt: " input
+    if [[ -z "$input" && -n "$default" ]]; then
+        input=$default
+    fi
+    echo "$input"
 }
 
 # read until not empty when input is empty
 # $1: prompt
 # $2: default
 function must_read_if_empty {
-	local prompt=$1
-	local input=$2
-	if [[ -z "$input" ]]; then
-		input=$(must_read "$prompt")
-	fi
-	echo "$input"
+    local prompt=$1
+    local input=$2
+    if [[ -z "$input" ]]; then
+        input=$(must_read "$prompt")
+    fi
+    echo "$input"
 }
 
 # read until not empty
 # $1: prompt
 function must_read {
-	local prompt=$1
-	local input=''
-	read -p "> $prompt: " input
-	while [[ -z "$input" ]]; do
-		read -p "> $prompt: " input
-	done
-	echo "$input"
+    local prompt=$1
+    local input=''
+    read -p "> $prompt: " input
+    while [[ -z "$input" ]]; do
+        read -p "> $prompt: " input
+    done
+    echo "$input"
 }
 
 # confirm, exit if not confirmed
 # $1: 1 treat empty as confirmed
 function confirm {
-	local input=""
-	read -p "> confirm(y/N): " input
-	if [ -z "$input" ]; then
-		if [ "$1" = 1 ]; then
-			return
-		fi
-	fi
-	if [ "$input" != "y" ]; then
-		echo_stderr "exit..."
-		exit 0
-	fi
+    local input=""
+    read -p "> confirm(y/N): " input
+    if [ -z "$input" ]; then
+        if [ "$1" = 1 ]; then
+            return
+        fi
+    fi
+    if [ "$input" != "y" ]; then
+        echo_stderr "exit..."
+        exit 0
+    fi
+}
+
+function confirm_v {
+    local input=""
+    read -p "> confirm(y/N): " input
+    if [ -z "$input" ]; then
+        if [ "$1" = 1 ]; then
+            return 0
+        fi
+    fi
+    if [ "$input" != "y" ]; then
+        return 1
+    fi
+    return 0
 }
 
 # stop and wait for any key
 function press_any_key {
-	read -p "Press any key to continue"
+    read -p "Press any key to continue"
 }
 
 # echo to stderr
 # $@: echo args
 function echo_stderr {
-	echo "$@" >&2
+    echo "$@" >&2
 }
 
 # change color to red foreground
 # $@: text
 function red_fg {
-	echo -e "\033[31m$@\033[0m"
+    echo -e "\033[31m$@\033[0m"
+}
+
+function yellow_fg {
+    echo -e "\033[33m$@\033[0m"
+}
+
+function green_fg {
+    echo -e "\033[32m$@\033[0m"
 }
 
 # change color to red background and black foreground
 # $@: text
 function red_bg_black_fg {
-	echo -e "\033[41;30m$@\033[0m"
+    echo -e "\033[41;30m$@\033[0m"
 }
 
 # cut prefix
@@ -150,9 +172,9 @@ function red_bg_black_fg {
 # $2: prefix
 # WARN: if $str has blank, use "" to wrap it like: cut_prefix "$str" "$prefix"
 function cut_prefix {
-	local str=$1
-	local prefix=$2
-	echo ${str#$prefix}
+    local str=$1
+    local prefix=$2
+    echo ${str#$prefix}
 }
 
 # cut suffix
@@ -160,20 +182,20 @@ function cut_prefix {
 # $2: suffix
 # WARN: if $str has blank, use "" to wrap it like: cut_prefix "$str" "$prefix"
 function cut_suffix {
-	local str=$1
-	local suffix=$2
-	echo ${str%$suffix}
+    local str=$1
+    local suffix=$2
+    echo ${str%$suffix}
 }
 
 # format json for http
 # $1: text
 function fmt_json_for_http {
-	if [ -z "$1" ]; then
-		echo "null"
-	elif check_command_exists 'jq'; then
-		echo $(jq -n --arg msg "$1" '$msg')
-	else
-		echo_stderr "[fmt_json_for_http] jq not found, use raw text"
-		echo "$1"
-	fi
+    if [ -z "$1" ]; then
+        echo "null"
+    elif check_command_exists 'jq'; then
+        echo $(jq -n --arg msg "$1" '$msg')
+    else
+        echo_stderr "[fmt_json_for_http] jq not found, use raw text"
+        echo "$1"
+    fi
 }
